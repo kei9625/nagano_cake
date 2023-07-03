8c1c29efdcceca3a2527e0bc59cf3a14d769d6b1
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-07-03 08:49:25 +0000

    top and about 画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,38 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
+    get 'homes/top'
     get 'items/index'
     get 'items/new'
     get 'items/show'
     get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/check'
-    get 'orders/thanks'
-    get 'orders/index'
     get 'orders/show'
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
   end
-  namespace :public do
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+    get '/customers/sign_up' => 'registrations#new'
+    get '/customers/sign_in' => 'sessions#new'
+
+    resources :customers, only: [:show, :edit, :update]
+    get '/customers/confirm_withdraw' => 'customers#confirm_withdraw'
+    patch '/customers/withdraw' => 'customers#withdraw'
+    get 
+    get 
+
+    resources :items, only: [:index, :show]
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/check' => 'orders#check'
+    get 'orders/thanks' => 'orders#thanks'
+
     get 'cart_items/index'
+
   end
+
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers, skip: [:passwords], controllers: {
