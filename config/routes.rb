bda874c699fd05709601c2fea39d6f779a63dd55
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-07-04 09:21:37 +0000

    [finish] customer sign_in and up. need bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
   namespace :admin do
     get 'homes/top'
     get 'items/index'
@@ -13,42 +25,25 @@ Rails.application.routes.draw do
   end
 
   scope module: :public do
-
     root to: 'homes#top'
     get '/about' => 'homes#about'
-    get '/customers/sign_up' => 'registrations#new'
-    get '/customers/sign_in' => 'sessions#new'
-
-    resources :customers, only: [:update]
     get '/customers/my_page' => 'customers#show'
-    get '/customers/information/edit' => 'customers#show'
+    get '/customers/information/edit' => 'customers#edit'
     get '/customers/confirm_withdraw' => 'customers#confirm_withdraw'
     patch '/customers/withdraw' => 'customers#withdraw'
+    resource :customers, only: [:update]
 
     resources :items, only: [:index, :show]
 
-    resources :cart_items, only: [:index, :create, :update, :destroy]
     delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :cart_items, only: [:index, :create, :update, :destroy]
 
-    resources :orders, only: [:new, :create, :index, :show]
     post 'orders/check' => 'orders#check'
     get 'orders/thanks' => 'orders#thanks'
-
-
-
+    resources :orders, only: [:new, :create, :index, :show]
 
   end
 
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-}
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
