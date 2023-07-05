b159befec683237aab38d238ea643e49dcecf769
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-07-05 09:32:12 +0000

    [finish]ログイン、アウトの動作確認。マイページは作成中

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,16 @@
 # 複数権限でのDevise使用・退会機能の実装
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+# before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
 
   # GET /resource/sign_in
   # def new
