dfe30a53ce660368fae9a7109711449e74612a46
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-07-07 07:41:48 +0000

    [finish]体調不良のため中断

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,7 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 # before_action :configure_permitted_parameters, if: :devise_controller?
+before_action :customer_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     root_path
@@ -10,6 +11,16 @@ class Public::SessionsController < Devise::SessionsController
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  protected
+
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if (@customer.valid_password?(params[:customer][:password]) && (@customer.customer_status == true))
+      redirect_to new_customer_registration_path
+    end
   end
 
 
