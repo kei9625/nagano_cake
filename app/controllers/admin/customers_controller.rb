b7f3681a4e21372ac66398677571c5541e501cab
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-07-12 08:31:51 +0000

    {WIP] admin会員一覧の表示は完了。ステータス表示は未完成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,21 @@
 class Admin::CustomersController < ApplicationController
+  
   def index
+    @all_customer = Customer.all
   end
 
   def show
+
   end
 
   def edit
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number )
+
+  end
+
 end
