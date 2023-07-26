5f61b4f2d9e4f1ef627a670842edf16a5eaa6a76
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-07-26 09:09:23 +0000

    注文確認画面の金額の合計の出し方の作業中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,25 +5,38 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    
+    @order = Order.new(order_params)
+    @customer = current_customer
+    @order.customer_id = current_customer.id
+    # binding.pry
   end
 
   def check
+    @cart_items = current_customer.cart_items
+    @order = Order.new(order_params)
+    @order.shipping_fee = 800
     
+    @order = Order.new(order_params)
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
   end
 
   def thanks
+
   end
 
   def index
+
   end
 
   def show
+
   end
 
   private
   def order_params
-    params.require(:order)
+    params.require(:order).permit(:postal_code, :address, :full_name, :payment_method, :total_due, :shipping_fee)
 
   end
 
